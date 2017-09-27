ebc31ce8fbb50569c814877e1b8c92c706bcbd6f
Hiswe <hiswehalya@gmail.com>
2017-09-27 10:27:22 +0200

    SEARCH – fix

--- js/search.jsx
+++ js/search.jsx
@@ -11,7 +11,7 @@ const Search = props => {
     <CharSection title="search">
       <form className="search">
         {/* not using a inpu[type="search"] because harder to style '¬_¬  */}
-        <input required type="text" onChange={ handleSearch } className="search__input" placeholder="search" />
+        <input required type="text" onInput={ handleSearch } className="search__input" placeholder="search" />
         <button className="search__reset" onClick={ handleReset } type="reset">
           <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
               <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
