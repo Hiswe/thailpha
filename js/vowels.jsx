1656ac699ac34592931be597137a38b1026fe480
Hiswe <hiswehalya@gmail.com>
2018-01-23 18:27:43 +0700

    CHARS – add missing glides and diphtongs

--- js/vowels.jsx
+++ js/vowels.jsx
@@ -1,29 +1,51 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 
 import CharSection from './char-section.jsx'
 import CharSubsection from './char-subsection.jsx'
 import CharList from './char-list.jsx'
 
-const Vowels = ({shorts, longs, diphtongAndMisc}) => (
-  <CharSection title="vowels">
-    <CharSubsection title="short">
-      <CharList chars={ shorts } />
-    </CharSubsection>
-    <CharSubsection title="long">
-      <CharList chars={ longs } />
-    </CharSubsection>
-    <CharSubsection title="diphtong & misc">
-      <CharList chars={ diphtongAndMisc } />
-    </CharSubsection>
-  </CharSection>
+const Vowels = ( chars ) => (
+  <Fragment>
+    <CharSection title="vowels">
+      <CharSubsection title="short">
+        <CharList chars={ chars.vowelsShorts } />
+      </CharSubsection>
+      <CharSubsection title="long">
+        <CharList chars={ chars.vowelsLongs } />
+      </CharSubsection>
+    </CharSection>
+    <CharSection title="diphtongs">
+      <CharSubsection title="short">
+        <CharList chars={ chars.diphtongsShorts } />
+      </CharSubsection>
+      <CharSubsection title="long">
+        <CharList chars={ chars.diphtongsLongs } />
+      </CharSubsection>
+    </CharSection>
+    <CharSection title="glides">
+      <CharSubsection title="short">
+        <CharList chars={ chars.glidesShorts } />
+      </CharSubsection>
+      <CharSubsection title="long">
+        <CharList chars={ chars.glidesLongs } />
+      </CharSubsection>
+    </CharSection>
+    <CharSection title="miscellanous">
+        <CharList chars={ chars.miscellanous } />
+    </CharSection>
+  </Fragment>
 )
 
 const mapStateToProp = state => {
   return {
-    shorts: state.chars.filter( char => char.isShort ),
-    longs:  state.chars.filter( char => char.isLong ),
-    diphtongAndMisc:  state.chars.filter( char => char.isDiphtongOrMisc ),
+    vowelsShorts:     state.chars.filter( char => char.isVowel && char.isShort ),
+    vowelsLongs:      state.chars.filter( char => char.isVowel && char.isLong ),
+    diphtongsShorts:  state.chars.filter( char => char.isDiphtong && char.isShort ),
+    diphtongsLongs:   state.chars.filter( char => char.isDiphtong && char.isLong ),
+    glidesShorts:     state.chars.filter( char => char.isGlide && char.isShort ),
+    glidesLongs:      state.chars.filter( char => char.isGlide && char.isLong ),
+    miscellanous:     state.chars.filter( char => char.isMiscellanous ),
   }
 }
 
